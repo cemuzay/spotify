d56e143a6167bce54c9c2072761348f00e1f8dcc
cem <85490666+cemuzay@users.noreply.github.com>
2023-08-03 05:11:27 +0300

    LearnReactnativeTasarım

--- App.js
+++ App.js
@@ -1,12 +1,77 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, SafeAreaView, TextInput, Image, ScrollView } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 export default function App() {
+  const [searchTerm, setSearchTerm] = useState('');
+
+  const handleInputChange = (value) => {
+    setSearchTerm(value);
+    // Perform your search logic here
+  };
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <ScrollView>
+      <View style={styles.searchContainer}>
+        <TextInput
+          style={styles.input}
+          placeholder="Friday, 15 Dec"
+          value={searchTerm}
+          onChangeText={handleInputChange}
+        />
+        <Icon name="search" size={20} color="#ccc" style={styles.searchIcon} />
+      </View>
+      <View style={styles.Learn}>
+        <Text style={styles.dim}>Learn</Text>
+        <Text style={styles.Choose}>Choose the part of the body</Text>
+      </View>
+      
+      <View style={[styles.boxcont, styles.tomato]}>
+        <View style={styles.infoContainer}>
+          <Text style={styles.tom}>Head & Face</Text>
+          <Text style={styles.dis}>11 diseases</Text>
+        </View>
+        <Image 
+          source={{
+            uri: '/Users/cemuzay/Desktop/image',
+          }}
+          style={styles.image}
+        />
+      </View>
+
+      <View style={[styles.boxcont, styles.green]}>
+        <Text style={styles.tom}>Head & Face</Text>
+        <Text style={styles.dis}>11 diseases</Text>
+        <Image 
+          source={{
+            uri: '/Users/cemuzay/Desktop/image2',
+          }}
+          style={styles.image}
+        />
+      </View>
+      <View style={[styles.boxcont, styles.sandy]}>
+        <Text style={styles.tom}>Head & Face</Text>
+        <Text style={styles.dis}>11 diseases</Text>
+        <Image 
+          source={{
+            uri: '/Users/cemuzay/Desktop/image3',
+          }}
+          style={styles.image}
+        />
+      </View>
+      <View style={[styles.boxcont, styles.blue]}>
+        <Text style={styles.tom}>Head & Face</Text>
+        <Text style={styles.dis}>11 diseases</Text>
+        <Image 
+          source={{
+            uri: '/Users/cemuzay/Desktop/image4',
+          }}
+          style={styles.image}
+        />
+      </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 }
 
@@ -14,7 +79,75 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+  },
+  searchContainer: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    marginBottom: 5,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 8,
+    paddingHorizontal: 10,
+    marginHorizontal: 20,
+    marginVertical: 20,
+  },
+  searchIcon: {
+    marginRight: 10,
+    borderRadius: 10,
+  },
+  input: {
+    flex: 1,
+    height: 45,
+  },
+  Learn: {
+    marginHorizontal: 25,
+  },
+  dim: {
+    fontSize: 40,
+    fontWeight: 'bold',
+  },
+  Choose: {
+    fontSize: 20,
+    marginVertical: 2,
+  },
+  tom: {
+    color: '#fff',
+    paddingLeft: 20,
+  },
+  boxcont: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    borderRadius: 30,
+    justifyContent: 'space-between', // Space evenly along the horizontal axis
+    marginHorizontal: 12,
+    paddingBottom: 5,
+    fontSize: 20,
+    marginVertical: 10,
+  },
+  dis: {
+    paddingLeft: 20,
+    color: 'gainsboro',
+  },
+  green: {
+    backgroundColor: '#8dc4bb',
+  },
+  sandy: {
+    backgroundColor: '#f2982f',
+  },
+  blue: {
+    backgroundColor: '#327389',
+  },
+  tomato: {
+    backgroundColor: '#eb7662',
+  },
+  infoContainer: {
+    flex: 1,
+  },
+  image: {
+    width: 90,
+    height: 90,
+    borderRadius:10,
+    marginHorizontal:20,
+    marginTop:10,
   },
 });
